54d2c3dc5f92f31ffd9fbe828b47f0830b4ab992
Henry Riddall <henryriddall@gmail.com>
2021-07-08 12:15:49 +0100

    Implemented logging

--- src/API/APICallFunctions.ts
+++ src/API/APICallFunctions.ts
@@ -1,22 +1,52 @@
 import {Rover, Camera, Photo} from './APIInterfaces'
 import axios from 'axios'
+import { configure, getLogger } from "log4js";
+
+configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs/debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'debug'}
+    }
+});
+
+const logger = getLogger("./index.ts");
 
 export async function getRovers(): Promise<Rover[]> {
     try {
         const response = await axios.get('http://finwhale.zoo.lan:8000/rovers')
-        const rovers: Rover[] = response.data.rovers
-        return rovers
+        if (response.status === 200){
+            const rovers: Rover[] = response.data.rovers
+            return rovers
+        }
+        else {
+            logger.warn("getRover hand non 200 status: " + response.data)
+            return []
+        }
     }
     catch (error) {
-
+        logger.warn("getRover call failed: " + error)
         return []
     }
 }
 
-export async function getPhotos(rover: Rover, camera: Camera) {
+export async function getPhotos(rover: Rover, camera: Camera): Promise<Photo[]> {
     const roverName: string = rover.name
     const cameraName: string = camera.name
-    const response = await axios.get(`http://finwhale.zoo.lan:8000/rovers/${roverName}/${cameraName}`)
-    const photos: Photo[] = response.data.photos
-    return photos
+    try {
+        const response = await axios.get(`http://finwhale.zoo.lan:8000/rovers/${roverName}/${cameraName}`)
+        if (response.status === 200){
+            const photos: Photo[] = response.data.photos
+            return photos
+        }
+        else {
+            logger.warn("getPhotos hand non 200 status: " + response.data)
+            return []
+        }
+    }
+    catch (error) {
+        logger.warn("getPhotos call failed: " + error)
+        return []
+    }
 }
