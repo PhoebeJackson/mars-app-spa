dc88f0edafde4cd5c18f90b74bf3eb02f198dde8
Phoebe Jackson <Phoebe.Jackson@softwire.com>
2021-07-09 12:28:53 +0100

    api request working and information displayable

--- src/components/pageTwo.tsx
+++ src/components/pageTwo.tsx
@@ -1,16 +1,25 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {getAsteroids} from "../API/APICallFunctions";
 import {Asteroid, Rover} from "../API/APIInterfaces";
 
 function PageTwo() {
-    const [asteroids, setAsteroids] = useState<{ [date:string] : Asteroid[]}>()
-    getAsteroids("2021-07-07").then((response) => {
-        setAsteroids(response)
-    })
+    const [asteroids, setAsteroids] = useState<Asteroid[]>( [] )
+    const listAsteroids = asteroids.map((asteroid) => {
+        if (asteroid.is_potentially_hazardous_asteroid) {
+            return <p key={asteroid.id}>{asteroid.name}</p>
+        }
+    });
+    useEffect(() => { //TODO - remove async and waiting
+        getAsteroids("2021-07-07").then(async (asteroidsResponse) => {
+            await setAsteroids(asteroidsResponse)
+        })
+    }, [])
+
     return (
         <section className="App">
             <header className="App-header">
                 Hi
+                <div>{listAsteroids}</div>
             </header>
         </section>
     );
